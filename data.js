e85166894307826af96588487242f283b51bf177
Phil Barresi <me@philbarresi.com>
2016-02-05 09:14:52 -0500

    Added data and server file

---
+++ data.js
@@ -0,0 +1,3 @@
+var exports = module.exports = {};
+
+// You can now add export properties to the exports object to be accessible from outside this file
---
+++ server.js
@@ -0,0 +1,18 @@
+// We first require our express package
+var express = require('express');
+var bodyParser = require('body-parser');
+var myData = require('./data.js')
+
+// This package exports the function to create an express instance:
+var app = express();
+
+// This is called 'adding middleware', or things that will help parse your request
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Setup your routes here!
+
+// We can now navigate to localhost:3000
+app.listen(3000, function () {
+    console.log('Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it');
+});
