8208ef578251c00d56337898914446e191fa84fe
Phil Barresi <me@philbarresi.com>
2016-02-11 17:00:52 -0500

    Updated boilerplate to support static assets and pages

--- server.js
+++ server.js
@@ -10,7 +10,19 @@ var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+// This middleware will activate for every request we make to 
+// any path starting with /assets;
+// it will check the 'static' folder for matching files 
+app.use('/assets', express.static('static'));
+
 // Setup your routes here!
+
+app.get("/", function (request, response) { 
+    // We have to pass a second parameter to specify the root directory
+    // __dirname is a global variable representing the file directory you are currently in
+    response.sendFile("./pages/index.html", { root: __dirname });
+});
+
 
 // We can now navigate to localhost:3000
 app.listen(3000, function () {
