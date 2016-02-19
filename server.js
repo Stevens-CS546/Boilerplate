3c03a08a0fcd680f1b1cce74b441d275defa6979
Phil Barresi <me@philbarresi.com>
2016-02-19 08:37:23 -0500

    Added home view to demonstrate templating

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // We first require our express package
 var express = require('express');
 var bodyParser = require('body-parser');
-var myData = require('./data.js')
+var myData = require('./data.js');
 
 // This package exports the function to create an express instance:
 var app = express();
+
+// We can setup Jade now!
+app.set('view engine', 'ejs');
 
 // This is called 'adding middleware', or things that will help parse your request
 app.use(bodyParser.json()); // for parsing application/json
@@ -17,12 +20,15 @@ app.use('/assets', express.static('static'));
 
 // Setup your routes here!
 
+app.get("/home", function (request, response) {
+    response.render("pages/home", { pageTitle: "Welcome Home" });
+});
+
 app.get("/", function (request, response) { 
     // We have to pass a second parameter to specify the root directory
     // __dirname is a global variable representing the file directory you are currently in
     response.sendFile("./pages/index.html", { root: __dirname });
 });
-
 
 // We can now navigate to localhost:3000
 app.listen(3000, function () {
